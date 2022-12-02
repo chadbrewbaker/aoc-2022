1166fc39070396a4d7991010df77581860aeb59e
Chad Brewbaker <crb002@Chads-MacBook-Air.local>
2022-12-01 22:19:33 -0600

    added zig compile opt flag

--- 01/bench.sh
+++ 01/bench.sh
@@ -3,7 +3,7 @@ rustc   -C opt-level=3 -C panic=abort -C  codegen-units=1  -o calories-v1-rs cal
 strip calories-v1-rs 
 
 rm calories-v1-zig
-zig build-exe calories-v1.zig --name calories-v1-zig
+zig build-exe -O ReleaseFast calories-v1.zig --name calories-v1-zig
 strip calories-v1-zig
 
 hyperfine './calories-v1-zig input.txt' 'python3 calories-v1.py input.txt' './calories-v1-rs input.txt'
